fcb9cbeb19ac90fced6c1fd955053c819391b927
lifei128 <hougeba@163.com>
2016-08-03 18:57:40 +0800

    fix

---
+++ src/com/lifeibigdata/algorithms/blog/LongSubstringK.java
@@ -0,0 +1,43 @@
+package com.lifeibigdata.algorithms.blog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lifei on 16/8/3.
+ */
+public class LongSubstringK {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstringKDistinct("eceba",3));
+    }
+    static int lengthOfLongestSubstringKDistinct(String s,int k){
+        int maxLen = 0;
+        int i,j = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        char c;
+        for (i = 0; i <s.length();i++){
+            while (j < s.length()){
+                c = s.charAt(j);
+                if (map.containsKey(c)){
+                    map.put(c,map.get(c)+1);
+                } else {
+                    if (map.size() == k)
+                        break;
+                    map.put(c,1);
+                }
+            j++;
+            }
+            maxLen = Math.max(maxLen,j - i);
+            c = s.charAt(i);
+            if (map.containsKey(c)){
+                int count = map.get(c);
+                if (count > 1){
+                    map.put(c,count -1);
+                } else {
+                    map.remove(c);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
---
+++ src/com/lifeibigdata/algorithms/blog/Root2Leaf.java
@@ -0,0 +1,49 @@
+package com.lifeibigdata.algorithms.blog;
+
+import com.lifeibigdata.algorithms.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lifei on 16/8/3.
+ */
+public class Root2Leaf {
+
+    static List<String> paths=new ArrayList<>();
+    public static void main(String[] args) {
+        TreeNode p1=new TreeNode(1);
+        TreeNode p2=new TreeNode(2);
+        TreeNode p3=new TreeNode(3);
+        TreeNode p4=new TreeNode(4);
+        p1.left=p2;
+        p1.right=p3;
+        p2.left=p4;
+
+        binaryTreePaths(p1);
+    }
+    public static List<String> binaryTreePaths(TreeNode root) {
+
+        if(root==null)
+            return paths;
+        String curPath="";
+        serachTree(root, curPath);
+        return paths;
+    }
+    public static void serachTree(TreeNode root,String currentPath) {
+
+        currentPath+=root.val;
+        if(root.left==null&&root.right==null)
+        {
+            paths.add(currentPath);
+            System.out.println(currentPath);
+            return;
+        }
+        currentPath+="->";
+        if(root.left!=null)
+            serachTree(root.left, currentPath);
+        if(root.right!=null)
+            serachTree(root.right, currentPath);
+    }
+
+}
